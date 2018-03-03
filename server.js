54ae82142bedee58edac6937b90b9ce4e9727c4b
Krishnam Raju Relangi <rkraju72@users.noreply.github.com>
2018-03-03 13:06:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 
 var pool = new Pool(config);
 /*
-app.get('/test-db', function (Req,res) {
+app.get('/test-db', function (req,res) {
    pool.query('SELECT * FROM test', function(err, result){
        if(err) {
            res.status(500).send(err.toString());
